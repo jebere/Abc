6af1488183b35ee2e2c02f2d211aec247f71bf11
Jelizaveta Beresnjeva <jebere@ttu.ee>
2020-03-11 22:06:23 +0200

    repositori interfeicid sai refaktooritud

--- Domain/Common/IRepository.cs
+++ Domain/Common/IRepository.cs
@@ -3,18 +3,12 @@ using System.Threading.Tasks;
 
 namespace Abc.Domain.Common
 {
-    public interface IRepository<T>
+    public interface IRepository<T>: ICrudMethods<T>, IPaging, ISearching, ISorting
     {
-        Task<List<T>> Get();
-        Task<T> Get(string id);
-        Task Delete(string id);
-        Task Add(T obj);
-        Task Update(T obj);
-        string SortOrder { get; set; }
-        string SearchString { get; set; }
-        int PageIndex { get; set; }
-        bool HasNextPage { get; set; }
-        bool HasPreviousPage { get; set; }
+        
+       
+       
+        
 
     }
 }
